2f6ab8d6dc014cfe20e06f056df9fb863876fbd8
Harshita Mugdha <harshitamugdha@gmail.com>
2025-10-10 11:18:42 +0530

    6th commit

---
+++ sumofsubarray.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[3]={1,2,3};
+    // for(int i=0;i<3;i++){
+    //     for(int j=i;j<3;j++){
+    //         int c=0;
+    //         for(int k=i;k<=j;k++){
+    //             c+=arr[k];
+    //         }
+    //         cout<<c<<" ";
+    //         cout<<endl;
+    //     }
+    //     }
+    for(int i=0;i<3;i++){
+        int c=0;
+       for(int j=i;j<3;j++){
+           c+=arr[j];
+           cout<<c<<" ";
+
+       }
+    }
+}
+
+
+    

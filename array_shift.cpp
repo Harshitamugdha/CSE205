95eb8f3f9a857470ad60f5a42d6c0444ab0c9bda
Harshita Mugdha <harshitamugdha@gmail.com>
2025-10-13 14:29:16 +0530

    9th commit

--- array_shift.cpp
+++ array_shift.cpp
@@ -1,16 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int arr[6]={0,10,20,30,40,50};
-    int n=6;
-    for(int i=n-1;i>0;i--){
-        if(i==0){
-            arr[i]=0;
-        }
-        else{
-             arr[i]=arr[i-1];
-        }
+    int arr[5]={10,20,30,40,50};
+    int n=5;
+    // for(int i=n-1;i>0;i--){
+    //     if(i==0){
+    //         arr[i]=0;
+    //     }
+    //     else{
+    //          arr[i]=arr[i-1];
+    //     }
+    // }
+    // for(int i=0;i<n-1;i++){
+        
+    //     arr[i]=arr[i+1];
+    // }
+    for(int i=2;i<n-1;i++){
+            arr[i]=arr[i+1];
     }
+    arr[n-1]=0;
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
--- sort.cpp
+++ sort.cpp
@@ -1,9 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    vector <int> arr={1,1,1,0,0,2,1,0,2};
-    
-     for(int i=0;i<arr.size();i++){
-        cout<<arr[i]<<" ";
+    vector <int> nums={2,0,2,1,1,0};
+   int s=0;
+   int e=nums.size()-1;
+   int m=0;
+   while(m<=e){
+    if(nums[m]==2){
+        swap(nums[m],nums[e]);
+        e--;
+    }
+    else if(nums[m]==1){
+        m++;
+    }
+    else if(nums[m]==0){
+        swap(nums[m],nums[s]);
+        s++;
+        m++;
+    }
+   }
+     for(int i=0;i<nums.size();i++){
+        cout<<nums[i]<<" ";
     }
 }

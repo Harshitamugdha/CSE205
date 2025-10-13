a866789c9cac804ebe45240c1f26906f65772817
Harshita Mugdha <harshitamugdha@gmail.com>
2025-10-13 13:11:17 +0530

    8th commit

---
+++ sort.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    vector <int> arr={1,1,1,0,0,2,1,0,2};
+    
+     for(int i=0;i<arr.size();i++){
+        cout<<arr[i]<<" ";
+    }
+}

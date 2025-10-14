116edc9cff3a65459a748ac23c0f5fd1868f1138
Harshita Mugdha <harshitamugdha@gmail.com>
2025-10-14 15:07:15 +0530

    10th commit

---
+++ dup_element.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int nums[7]={1,2,5,4,3,6,7};
+    int maxE=nums[0];
+        for(int i=0;i<7;i++){
+            if(nums[i]>maxE){
+                maxE=nums[i];
+            }
+        }
+        vector <int> freq(maxE+1,0);
+        for(int i=0;i<=maxE;i++){
+            freq[nums[i]]++;
+        }
+        for(int i = 0 ; i<maxE+1;i++){
+            if(freq[i]>1) return i;
+        }
+}
